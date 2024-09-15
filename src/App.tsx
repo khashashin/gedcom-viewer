068f56d61609706ccc88596617b496a2565d1763
Yusuf Khasbulatov <khashashin@gmail.com>
2024-09-15 17:00:23 +0200

    Add GitHub repository link in the footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
 import React from "react";
 import EditableView from "./components/EditableView";
 import packageJson from "../package.json";
+import { GitHubLogoIcon } from "@radix-ui/react-icons";
 
 const App: React.FC = () => {
   return (
     <div>
-      <div className="absolute bottom-2 right-2 text-sm text-gray-700">
-        Version {packageJson.version}
-      </div>
-
       <EditableView />
+      <div className="absolute bottom-2 right-2 text-sm text-gray-700 flex items-center space-x-2">
+        <a
+          href="https://github.com/khashashin/gedcom-viewer"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="flex items-center"
+        >
+          <GitHubLogoIcon className="text-xl mr-1" />
+        </a>
+        <span>Version {packageJson.version}</span>
+      </div>
     </div>
   );
 };
